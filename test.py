90c54079edf13a8a396a0448d7357a75566212a4
Denis <denysvedernikov333@gmail.com>
2021-12-07 03:56:51 +0200

    Adding Rabin-Karp Algorithm

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-import main
+import RabinKarp
 import unittest
 
 class Test(unittest.TestCase):
-    def test_wchain(self):
-        self.assertEqual(main.wchain(['testt', 'tett', 'tet', 'randomword', 's']), 3)
+    def test_RabinCarp(self):
+        pattern = "test"
+        with open("test.txt", encoding="utf-8") as file_read:
+            main_text = file_read.read()
 
+        self.assertEqual(RabinKarp.search_patterns_in_text(main_text, pattern), 5)
